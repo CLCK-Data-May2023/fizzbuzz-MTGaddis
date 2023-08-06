f66c451e7e2c6a425b1786322a7d6880185d77db
Michael Gaddis <mgaddis88@gmail.com>
2023-08-06 04:59:21 -0400

    Redoing commit, maybe didn't do it correctly before

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,9 @@
-# add your code here
-
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+      print("Fizzbuzz")
+    elif number % 3 == 0:
+       print("Fizz")
+    elif number % 5 == 0:
+       print("Buzz")
+    else:
+       print(number)
